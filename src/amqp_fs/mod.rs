7149a0ce2543d65602a07f6b3f82a077b59b6f29
Erik Stokes <stokes.erik@gmail.com>
2022-05-01 11:29:00 -0400

    Lookup and mkdir work now

--- src/amqp_fs/mod.rs
+++ src/amqp_fs/mod.rs
@@ -1,89 +1,231 @@
+#![allow(unused_imports)]
 
 use std::{io::{self, BufRead},
           os::unix::prelude::*,
           time::Duration,
+          sync::atomic::{AtomicU64, Ordering},
+          sync::Arc,
+          mem,
+          fmt,
+          cell::RefCell,
+          rc::Rc,
 };
+use libc::stat;
+use std::ops::Deref;
+
+use std::collections::hash_map::{Entry, HashMap, RandomState};
 use polyfuse::{
     op,
     reply::{AttrOut, EntryOut, FileAttr, ReaddirOut, WriteOut},
-    Operation,
     Request,
 };
 use dashmap::DashMap;
 
 use lapin::{BasicProperties,
             ConnectionProperties,
-            message::DeliveryResult,
+            // message::DeliveryResult,
             options::*,
-            publisher_confirm::Confirmation,
-            types::{FieldTable, ShortString},
+            // publisher_confirm::Confirmation,
+            types::ShortString,
 };
 
 use tokio_amqp::*;
-use pinky_swear::PinkySwear;
-use tracing::{info, warn, error, instrument};
+// use pinky_swear::PinkySwear;
+#[allow(unused_imports)] use tracing::{info, warn, error, debug};
+// use tracing_subscriber::fmt;
+mod connection;
 
-const TTL: Duration = Duration::from_secs(60 * 60 * 24 * 365);
+const TTL: Duration = Duration::from_secs(1);
 const ROOT_INO: u64 = 1;
 const HELLO_INO: u64 = 2;
 const HELLO_FILENAME: &str = "hello";
+type Ino = u64;
 
-mod connection;
 
+
+#[derive(Clone)]
+struct DirEntry {
+    name: String,
+    ino: Ino,
+    typ: u32,
+    parent_ino: Ino,
+    // Names of child entries and their inodes. This does /not/
+    // contain the "." and ".." entries of directory.
+    children: DashMap<String, Ino, RandomState>,
+    attr: libc::stat,
+}
+
+// type RegularFile = Vec<u8>;
+
+struct DirectoryTable {
+    map: DashMap<Ino, DirEntry, RandomState>,
+    next_ino: AtomicU64,
+}
+
+impl DirEntry {
+    fn root(uid: u32, gid: u32, mode: u32) -> Self {
+        let r = Self {
+            name: ".".to_string(),
+            ino: ROOT_INO,
+            typ: libc::DT_DIR as u32,
+            parent_ino: ROOT_INO,
+            attr: {
+                let mut attr = unsafe {mem::zeroed::<libc::stat>() };
+                attr.st_ino = ROOT_INO;
+                attr.st_nlink = 1; // that's right, 1 not 2. The second link will be made when the directory table assembles itself
+                attr.st_mode = libc::S_IFDIR | mode;
+                attr.st_gid = gid;
+                attr.st_uid = uid;
+                attr
+            },
+            children: DashMap::with_hasher(RandomState::new()),
+        };
+        r.children.insert(".".to_string(), r.ino);
+        r
+    }
+
+}
+
+/// Main filesytem  handle. Representes  the connection to  the rabbit
+/// server and the one-deep list of directories inside it.
 pub(crate)
 struct Rabbit {
     connection: lapin::Connection,
-    entries: Vec<DirEntry>,
+    routing_keys: DirectoryTable,
     uid: u32,
     gid: u32,
+    ttl: Duration,
 }
 
-struct DirEntry {
-    name: &'static str,
-    ino: u64,
-    typ: u32,
+impl DirectoryTable {
+    fn new(root: &DirEntry) -> Self {
+        let map = DashMap::with_hasher(RandomState::new());
+        let dir_names = vec![".."];
+        // let mut ino = ROOT_INO;
+        // // Special case to create the default '.' and '..' directories.
+        // for name in dir_names.iter() {
+        //     debug!("Creating '{}' with inode {}", name, ino);
+        //     let dir_entry = match map.entry(ino) {
+        //         dashmap::mapref::entry::Entry::Occupied(..) => panic!("Unable to create root directory"),
+        //         dashmap::mapref::entry::Entry::Vacant(entry) => entry,
+        //     };
+        //     let mut attr  = unsafe { mem::zeroed::<libc::stat>() };
+        //     attr.st_ino = ino;
+        //     attr.st_nlink = 2;
+        //     attr.st_mode = libc::S_IFDIR | 0o700;
+
+        //     dir_entry.insert(DirEntry{
+        //         name: name.to_string(),
+        //         ino,
+        //         typ: libc::DT_DIR as u32,
+        //         attr,
+        //     });
+        //     ino += 1;
+        // }
+
+        let mut tbl = Self {
+            map,
+            next_ino: AtomicU64::new(ROOT_INO + 1),
+        };
+        tbl.map.insert(ROOT_INO, root.clone() );
+        for name in dir_names.iter() {
+            // If we can't make the root directory, the world is
+            // broken. Panic immediatly.
+            tbl.mkdir(name).unwrap();
+        }
+        // tbl.mkdir("abc");
+        tbl
+    }
+
+    /// Make a directory in the root. Note that subdirectories are not
+    /// allowed and so no parent is passed into this
+    fn mkdir(&mut self, name: &str) -> Result<libc::stat, libc::c_int> {
+        let ino = self.next_ino.fetch_add(1, Ordering::SeqCst);
+        info!("Creating directory {} with inode {}", name, ino);
+        let attr = match self.map.entry(ino) {
+            dashmap::mapref::entry::Entry::Occupied(..) => panic!("duplicate inode error"),
+            dashmap::mapref::entry::Entry::Vacant(entry) => {
+                let mut attr  = unsafe { mem::zeroed::<libc::stat>() };
+                attr.st_ino = ino;
+                attr.st_nlink = if name != "." {2} else {0};
+                attr.st_mode = libc::S_IFDIR | 0o700;
+                attr.st_blocks = 8;
+                attr.st_size = 4096;
+                let dir = DirEntry {
+                    name: name.to_string(),
+                    ino,
+                    parent_ino: ROOT_INO,
+                    typ: libc::DT_DIR as u32,
+                    attr,
+                    children: DashMap::with_hasher(RandomState::new()),
+                };
+                // self.child_map.insert(dir.ino, DashMap::new());
+                // self.child_map.get_mut(&ROOT_INO).unwrap().insert(&name, dir);
+                info!("Directory {} has {} children", dir.name, dir.children.len());
+                entry.insert(dir);
+                attr
+            }
+        };
+        self.map.entry(ROOT_INO).and_modify(|root| root.attr.st_nlink +=1);
+        self.map.get_mut(&ROOT_INO).unwrap().children.insert(name.to_string(), ino);
+        info!("Filesystem contains {} directories", self.map.len());
+        Ok(attr)
+    }
+
+    fn iter_dir<'a>(&'a self, dir: &'a DirEntry) -> impl Iterator<Item = (u64, DirEntry) > + '_ {
+        use dashmap::try_result::TryResult;
+        use std::iter;
+
+
+
+        debug!("Iterating over {} with {} children", dir.name, dir.children.len() );
+
+        dir.children.iter().filter_map(move |ino| {
+            match self.map.try_get(&ino) {
+                TryResult::Present(entry) => Some((ino.clone(), entry.clone() )),
+                TryResult::Absent => None,
+                TryResult::Locked => None,
+            }
+        })
+    }
 }
-
-type RegularFile = Vec<u8>;
-// type Ino = u64;
-
-// struct InodeTable {
-//     map: DashMap<Ino, INode, RandomState>,
-//     next_ino: AtomicU64,
-// }
 
 impl Rabbit {
     pub
-    async fn new(addr: &str) -> Self {
+    async fn new(addr: &str) -> Rabbit {
         // Add 3 entries by default. The normal "." and ".." and on
         // test entry named 'hello'
-        let mut entries = Vec::with_capacity(3);
-        entries.push(DirEntry {
-            name: ".",
-            ino: ROOT_INO,
-            typ: libc::DT_DIR as u32,
-        });
-        entries.push(DirEntry {
-            name: "..",
-            ino: ROOT_INO,
-            typ: libc::DT_DIR as u32,
-        });
-        entries.push(DirEntry {
-            name: HELLO_FILENAME,
-            ino: HELLO_INO,
-            typ: libc::DT_REG as u32,
-        });
+        // let mut routing_keys = Vec::with_capacity(3);
+        // routing_keys.push(DirEntry {
+        //     name: ".",
+        //     ino: ROOT_INO,
+        //     typ: libc::DT_DIR as u32,
+        // });
+        // routing_keys.push(DirEntry {
+        //     name: "..",
+        //     ino: ROOT_INO,
+        //     typ: libc::DT_DIR as u32,
+        // });
+        // entries.push(DirEntry {
+        //     name: HELLO_FILENAME,
+        //     ino: HELLO_INO,
+        //     typ: libc::DT_REG as u32,
+        // });
+        let uid = unsafe { libc::getuid() };
+        let gid = unsafe { libc::getgid() };
+        let root = DirEntry::root(uid, gid, 0o700);
 
-        Self {
+        Rabbit {
             connection: connection::get_connection(addr,
-                           ConnectionProperties::default().with_tokio()).await.unwrap(),
-            entries,
-            uid: unsafe { libc::getuid() },
-            gid: unsafe { libc::getgid() },
+                                                   ConnectionProperties::default().with_tokio()).await.unwrap(),
+            uid,
+            gid,
+            ttl: TTL,
+            routing_keys: DirectoryTable::new(&root),
         }
     }
 
-    fn fill_root_attr(&self, attr: &mut FileAttr) {
+    fn fill_dir_attr(&self, attr: &mut FileAttr) {
         attr.ino(ROOT_INO);
         attr.mode(libc::S_IFDIR as u32 | 0o555);
         attr.nlink(2);
@@ -91,7 +233,7 @@ impl Rabbit {
         attr.gid(self.gid);
     }
 
-    fn fill_routing_attr(&self, attr: &mut FileAttr) {
+    fn fill_file_attr(&self, attr: &mut FileAttr) {
         attr.ino(HELLO_INO);
         attr.size(0);// files always appear empty
         attr.mode(libc::S_IFREG as u32 | 0o666);
@@ -103,39 +245,70 @@ impl Rabbit {
     pub
     async fn lookup(&self, req: &Request, op: op::Lookup<'_>) -> io::Result<()> {
 
-        info!("Doing lookup of {:?}", op);
+        info!("Doing lookup of {:?} in parent inode {}", op.name(), op.parent());
 
-        match op.parent() {
-            ROOT_INO if op.name().as_bytes() == HELLO_FILENAME.as_bytes() => {
-                info!("You are looking up 'hello'");
+        use dashmap::mapref::entry::Entry;
+
+
+        match self.routing_keys.map.entry(op.parent()) {
+            Entry::Vacant(..) => {return req.reply_error(libc::ENOENT);},
+            Entry::Occupied(entry) => {
+                let parent = entry.get();
                 let mut out = EntryOut::default();
-                self.fill_routing_attr(out.attr());
-                out.ino(HELLO_INO);
-                out.ttl_attr(TTL);
-                out.ttl_entry(TTL);
-                req.reply(out)
+                out.ttl_attr(self.ttl);
+                out.ttl_entry(self.ttl);
+
+                // The name is a [u8] (i.e. a `char*`), so we have to cast it to unicode
+                let name = match op.name().to_str() {
+                    Some(name) => name,
+                    None => {return req.reply_error(libc::EINVAL);}
+                };
+
+                let ino = match parent.children.get(&name.to_string()) {
+                    Some(ino) => *ino,
+                    None => {return req.reply_error(libc::ENOENT);},
+                };
+                info!("Found inode {} for {}", ino, name);
+
+                match self.routing_keys.map.entry(ino) {
+                    Entry::Vacant(..) => { return req.reply_error(libc::ENOENT);},
+                    Entry::Occupied(entry) => {
+                        let dir = entry.get();
+                        out.ino(dir.ino);
+                        fill_attr(out.attr(), &dir.attr);
+                        req.reply(out)
+                   },
+                }
             }
-            _ => req.reply_error(libc::ENOENT),
         }
     }
 
     pub
     async fn getattr(&self, req: &Request, op: op::Getattr<'_>) -> io::Result<()> {
-        let fill_attr = match op.ino() {
-            ROOT_INO => Self::fill_root_attr,
-            HELLO_INO => Self::fill_routing_attr,
-            _ => return req.reply_error(libc::ENOENT),
+        info!("Getting attributes of {}", op.ino());
+
+        let entry = match self.routing_keys.map.entry(op.ino()) {
+            dashmap::mapref::entry::Entry::Occupied(entry) => entry,
+            dashmap::mapref::entry::Entry::Vacant(..) => {
+                // handle files here
+                info!("File does not exist");
+                return req.reply_error(libc::ENOENT);
+            }
         };
 
-        let mut out = AttrOut::default();
-        fill_attr(self, out.attr());
-        out.ttl(TTL);
+        // let fill_attr = Self::fill_dir_attr;
 
+        let mut out = AttrOut::default();
+        let dir = entry.get();
+        fill_attr(out.attr(), &dir.attr);
+        out.ttl(self.ttl);
+        debug!("getattr for {}: {:?}", dir.name, StatWrap::from(dir.attr));
         req.reply(out)
     }
 
     pub
     async fn read(&self, req: &Request, op: op::Read<'_>) -> io::Result<()> {
+        info!("Reading {} bytes from inode {}", op.size(), op.ino() );
         match op.ino() {
             HELLO_INO => (),
             ROOT_INO => return req.reply_error(libc::EISDIR),
@@ -144,43 +317,78 @@ impl Rabbit {
 
         let data: &[u8] = &[]; // Files are always empty
 
-        // let offset = op.offset() as usize;
-        // if offset < HELLO_CONTENT.len() {
-        //     let size = op.size() as usize;
-        //     data = &HELLO_CONTENT[offset..];
-        //     data = &data[..std::cmp::min(data.len(), size)];
-        // }
-
         req.reply(data)
     }
 
-    fn dir_entries(&self) -> impl Iterator<Item = (u64, &DirEntry)> + '_ {
-        self.entries.iter().enumerate().map(|(i, ent)| {
-            let offset = (i + 1) as u64;
-            (offset, ent)
+    fn dir_entries(&self) -> impl Iterator<Item = (u64, DirEntry)> + '_ {
+        self.routing_keys.map.iter().filter_map(|ent| {
+            // let i = ent.key();
+            let i:u64 = *ent.key();
+            let dir = ent.value();
+            debug!("Yielding directory {}", dir.name);
+            if dir.name != "." || dir.name!=".." {Some((i, dir.clone()))} else {None}
+            // (i as u64, ent)
         })
     }
 
     pub
     async fn readdir(&self, req: &Request, op: op::Readdir<'_>) -> io::Result<()> {
-        if op.ino() != ROOT_INO {
-            return req.reply_error(libc::ENOTDIR);
-        }
+        info!("Reading directory {} with offset {}", op.ino(), op.offset());
 
+        use dashmap::try_result::TryResult;
+
+        let dir = match self.routing_keys.map.try_get(&op.ino()) {
+            TryResult::Present(entry) => entry,
+            TryResult::Absent => {return req.reply_error(libc::ENOENT);}
+            TryResult::Locked => {return req.reply_error(libc::ENOENT);}
+        };
+
+        debug!("Looking for directory {} in parent {}", dir.ino, dir.parent_ino);
+
+
+        // Otherwise we are reading '.', so list all the directories.
+        // There are no top level files.
         let mut out = ReaddirOut::new(op.size() as usize);
 
-        for (i, entry) in self.dir_entries().skip(op.offset() as usize) {
+        for (i, entry) in self.routing_keys.iter_dir(&dir).skip(op.offset() as usize) {
+            info!("Found directory entry {} in {}", entry.name, op.ino() );
             let full = out.entry(
-                entry.name.as_ref(), //
+                entry.name.as_ref(),
                 entry.ino,
                 entry.typ,
-                i + 1,
+                i + 1, // offset
             );
             if full {
+                debug!("readdir request full");
                 break;
             }
         }
 
+        req.reply(out)
+    }
+
+    pub
+    async fn mkdir(&mut self, req: &Request, op: op::Mkdir<'_>) -> io::Result<()> {
+        let parent_ino = op.parent();
+        let name = op.name();
+        info!("Creating directory {:?} in parent {}", name, parent_ino);
+        let mut out = EntryOut::default();
+        let str_name = match name.to_str() {
+            Some(s) => s,
+            None => { error!("Invalid filename"); return req.reply_error(libc::EINVAL); }
+        };
+        let stat = match self.routing_keys.mkdir(str_name) {
+            Ok(attr) => attr,
+            _ => {return req.reply_error(libc::EEXIST); }
+        };
+        // out.attr().ino(stat.st_ino);
+        // out.attr().mode(libc::S_IFDIR | 0x700 as u32);
+        fill_attr(out.attr(), &stat);
+        out.ino(stat.st_ino);
+        out.ttl_attr(self.ttl);
+        out.ttl_entry(self.ttl);
+        // self.fill_dir_attr(out.attr());
+        info!("New directory has stat {:?}", StatWrap::from(stat) );
         req.reply(out)
     }
 
@@ -221,4 +429,45 @@ impl Rabbit {
         req.reply(out)
     }
 
+}
+
+fn fill_attr(attr: &mut FileAttr, st: &libc::stat) {
+    attr.ino(st.st_ino);
+    attr.size(st.st_size as u64);
+    attr.mode(st.st_mode);
+    attr.nlink(st.st_nlink as u32);
+    attr.uid(st.st_uid);
+    attr.gid(st.st_gid);
+    attr.rdev(st.st_rdev as u32);
+    attr.blksize(st.st_blksize as u32);
+    attr.blocks(st.st_blocks as u64);
+    attr.atime(Duration::new(st.st_atime as u64, st.st_atime_nsec as u32));
+    attr.mtime(Duration::new(st.st_mtime as u64, st.st_mtime_nsec as u32));
+    attr.ctime(Duration::new(st.st_ctime as u64, st.st_ctime_nsec as u32));
+}
+
+struct StatWrap {
+    stat: libc::stat,
+}
+
+impl From<libc::stat> for StatWrap {
+    fn from(s: libc::stat) -> StatWrap {
+        StatWrap{ stat:s }
+    }
+}
+
+impl fmt::Debug for StatWrap {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let stat = self.stat;
+        f.debug_struct("stat_t")
+            .field("st_ino", &stat.st_ino)
+            .field("st_size", &stat.st_size)
+            .field("st_nlink", &stat.st_nlink)
+            .field("uid", &stat.st_uid)
+            .field("gid", &stat.st_gid)
+            .field("rdev", &stat.st_rdev)
+            .field("st_blksize", &stat.st_blksize)
+            .field("st_blocks", &stat.st_blocks)
+            .finish()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,33 @@
 #![warn(clippy::all)]
 
-use anyhow::{ensure, Context as _, Result};
-use std::{ path::PathBuf, sync::Arc};
+use anyhow::{ensure, Result};
+use std::{ sync::Arc};
 use tokio::{
+    sync::Mutex,
     task::{self, JoinHandle},
 };
 
 use polyfuse::{KernelConfig, Operation};
 
-use tracing::{info, error, debug, Level};
+
+#[allow(unused_imports)] use tracing::{info, error, debug, Level};
 use tracing_subscriber;
 
 use clap::Parser;
 
 mod amqp_fs;
 mod session;
-
-
-#[derive(Parser)]
-struct Cli {
-    mountpoint: PathBuf,
-    #[clap(short, long, default_value_t = String::from("amqp://127.0.0.1:5671/%2f?auth_mechanism=external"))]
-    rabbit_addr: String,
-}
+mod cli;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
 
     // let mut args = pico_args::Arguments::from_env();
-    let args = Cli::parse();
+    let args = cli::Args::parse();
 
     // let mountpoint: PathBuf = args.free_from_str()?.context("missing mountpoint")?;
     ensure!(args.mountpoint.is_dir(), "mountpoint must be a directory");
@@ -39,21 +37,18 @@ async fn main() -> Result<()> {
 
     let session = session::AsyncSession::mount(args.mountpoint, KernelConfig::default()).await?;
 
-    // let addr = "amqp://127.0.0.1:5671/%2f?auth_mechanism=external";
-
-
-    let fs = Arc::new(amqp_fs::Rabbit::new(&args.rabbit_addr).await);
+    let fs = Arc::new(Mutex::new(amqp_fs::Rabbit::new(&args.rabbit_addr).await));
 
     while let Some(req) = session.next_request().await? {
-        let fs = fs.clone();
-
+        let mut fs = fs.clone();
         let _: JoinHandle<Result<()>> = task::spawn(async move {
             match req.operation()? {
-                Operation::Lookup(op) => fs.lookup(&req, op).await?,
-                Operation::Getattr(op) => fs.getattr(&req, op).await?,
-                Operation::Read(op) => fs.read(&req, op).await?,
-                Operation::Readdir(op) => fs.readdir(&req, op).await?,
-                Operation::Write(op, data) => fs.write(&req, op, data).await?,
+                Operation::Lookup(op) => fs.lock().await.lookup(&req, op).await?,
+                Operation::Getattr(op) => fs.lock().await.getattr(&req, op).await?,
+                Operation::Read(op) => fs.lock().await.read(&req, op).await?,
+                Operation::Readdir(op) => fs.lock().await.readdir(&req, op).await?,
+                Operation::Write(op, data) => fs.lock().await.write(&req, op, data).await?,
+                Operation::Mkdir(op) => fs.lock().await.mkdir(&req, op).await?,
                 _ => req.reply_error(libc::ENOSYS)?,
             }
 
