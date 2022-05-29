cf9cffad6da75524a2651a687cccccaee5abfc86
Erik Stokes <stokes.erik@gmail.com>
2022-05-29 11:42:25 -0400

    Add help strings for cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,25 +1,32 @@
 use clap::Parser;
 use std::path::PathBuf;
 
+/// Fuse filesytem that publishes to a RabbitMQ server
 #[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
 pub struct Args {
     pub(crate) mountpoint: PathBuf,
 
     #[clap(short, long, default_value_t = String::from("amqp://127.0.0.1:5671/%2f?auth_mechanism=external"))]
     pub(crate) rabbit_addr: String,
 
+    /// Exchange to bind directories in the mount point to
     #[clap(short, long, default_value_t = String::from(""))]
     pub(crate) exchange: String,
 
+    /// P12 formatted key
     #[clap(short, long)]
     pub(crate) key: String,
 
+    /// PEM formatted certificate chain
     #[clap(short, long)]
     pub(crate) cert: String,
 
+    /// Password for key, if encrypted
     #[clap(long)]
     pub(crate) password: Option<String>,
 
+    /// Maximum number of bytes to buffer in open files
     #[clap(short, long, default_value_t=16777216)]
     pub(crate) buffer_size: usize,
 }
