f097f3d78ea185f79fb68f3f7e59f1f389637799
Erik Stokes <stokes.erik@gmail.com>
2022-10-09 12:48:38 -0400

    Implement connection pooling
    Connections are lazily opened when files are opened and re-opened when closed

--- src/amqp_fs/connection.rs
+++ src/amqp_fs/connection.rs
@@ -1,13 +1,105 @@
 //! Functions for managing TLS rabbit connections
-use std::fs::File;
+use std::{fs::File, sync::Arc};
 use std::io::Read;
+
+use deadpool::{async_trait,
+               managed};
 
 use lapin::{Connection,
             uri::AMQPUri,
-            tcp::{AMQPUriTcpExt, NativeTlsConnector},
+            tcp::{AMQPUriTcpExt, NativeTlsConnector, HandshakeError}, TcpStream, ConnectionProperties,
 };
+use native_tls::{TlsConnectorBuilder, TlsConnector};
+use tracing::info;
 
 use crate::cli;
+
+
+type RecycleResult = managed::RecycleResult<lapin::Error>;
+type RecycleError = managed::RecycleError<lapin::Error>;
+
+pub(crate) struct ConnectionManager {
+    uri: AMQPUri,
+    properties: ConnectionProperties,
+    connector: Option<Arc<TlsConnector>>,
+}
+
+impl ConnectionManager {
+    fn new(uri: &str,
+           connector: Option<Arc<TlsConnector>>,
+           properties: ConnectionProperties) -> Self{
+        Self{uri: uri.clone().parse::<lapin::uri::AMQPUri>().unwrap(),
+             properties,
+             connector,
+             }
+    }
+
+    pub fn from_command_line(args: &cli::Args,
+                             properties: ConnectionProperties) -> Self {
+
+        let mut tls_builder = native_tls::TlsConnector::builder();
+        tls_builder.identity(identity_from_file(&args.tls_options.key,
+                                                &args.tls_options.password));
+        tls_builder.add_root_certificate(ca_chain_from_file(&args.tls_options.cert));
+        tls_builder.danger_accept_invalid_hostnames(true);
+        let connector = Arc::new(tls_builder.build().expect("tls connector"));
+
+        Self::new(&args.rabbit_addr,
+                  Some(connector),
+                  properties,
+        )
+
+
+    }
+
+
+    async fn get_connection(
+        &self,
+    ) -> lapin::Result<Connection> {
+        if let Some(connector) = self.connector.clone() {
+            let connect =  move | uri: &AMQPUri | {
+                uri.clone().connect().and_then(|stream| {
+                    stream.into_native_tls(
+                        &connector,
+                        // &builder.build().expect("tls config"),
+                        &uri.authority.host,
+                    )
+                })
+            };
+
+            Connection::connector(self.uri.clone(),
+                                  Box::new(connect),
+                                  self.properties.clone()).await
+        } else {
+            unimplemented!("Non-tls connections not implemented");
+}
+        }
+
+}
+
+#[async_trait]
+impl managed::Manager for ConnectionManager {
+    type Type = lapin::Connection;
+    type Error = lapin::Error;
+
+    async fn create(&self) ->  lapin::Result<Self::Type> {
+        info!("Opening new connection");
+        self.get_connection().await
+    }
+
+    // copypasta from https://github.com/bikeshedder/deadpool/blob/d7167eaf47ccaadabfb831ce3718cdebe51185ba/lapin/src/lib.rs#L91
+    async fn recycle(&self, conn: &mut lapin::Connection) -> RecycleResult {
+        match conn.status().state() {
+            lapin::ConnectionState::Connected => Ok(()),
+            other_state => Err(RecycleError::Message(format!(
+                "lapin connection is in state: {:?}",
+                other_state
+            ))),
+        }
+    }
+}
+
+pub(crate) type ConnectionPool = managed::Pool<ConnectionManager>;
 
 /// Load a TLS identity from p12 formatted file path
 fn identity_from_file(p12_file: &str, password: &Option<String>) -> native_tls::Identity {
@@ -32,29 +124,4 @@ fn ca_chain_from_file(pem_file: &str) -> native_tls::Certificate {
     f.read_to_end(&mut ca_chain)
         .expect("Unable to read ca chain");
     native_tls::Certificate::from_pem(&ca_chain).expect("unable to parse certificate")
-}
-
-/// Get a rabbit connection using the passed command line arguments,
-/// along with any extra properties
-pub async fn get_connection(
-    args: &cli::Args,
-    conn_props: lapin::ConnectionProperties,
-) -> lapin::Result<Connection> {
-    let uri = args.rabbit_addr.clone().parse::<lapin::uri::AMQPUri>().unwrap();
-
-    let mut tls_builder = native_tls::TlsConnector::builder();
-    tls_builder.identity(identity_from_file(&args.key, &args.password));
-    tls_builder.add_root_certificate(ca_chain_from_file(&args.cert));
-    tls_builder.danger_accept_invalid_hostnames(true);
-
-    let connect =  move | uri: &AMQPUri | {
-        uri.connect().and_then(|stream| {
-            stream.into_native_tls(
-                &tls_builder.build().expect("tls config"),
-                &uri.authority.host,
-            )
-        })
-    };
-
-    Connection::connector(uri.clone(), Box::new(connect), conn_props).await
 }
--- src/amqp_fs/mod.rs
+++ src/amqp_fs/mod.rs
@@ -73,7 +73,7 @@ const TTL: Duration = Duration::from_secs(1);
 /// Errors, despite no Rust `Err` ever being returned.
 pub(crate) struct Rabbit {
     /// Open RabbitMQ connection
-    connection: Arc<RwLock<lapin::Connection>>,
+    connection: Arc<RwLock<connection::ConnectionPool>>,
 
     /// [Self::write] will publish message to this exchnage
     exchange: String,
@@ -108,9 +108,10 @@ impl Rabbit {
             .with_reactor(tokio_reactor_trait::Tokio);
 
 
+        let mgr = connection::ConnectionManager::from_command_line(&args, conn_props);
         Rabbit {
             connection: Arc::new(RwLock::new(
-                connection::get_connection(args, conn_props).await.unwrap()
+               connection::ConnectionPool::builder(mgr).build().unwrap()
             )),
             uid,
             gid,
@@ -430,21 +431,28 @@ impl Rabbit {
         // This is the only place we touch the rabbit connection.
         // Creating channels is not mutating, so we only need read
         // access
-        let conn = self.connection.as_ref().read().await;
-        trace!("Creating new file handle");
-        let fh = match self
-            .file_handles
-            .insert_new_fh(
-                &conn,
-                &self.exchange,
-                &routing_key,
-                op.flags(),
-                &self.write_options,
-            )
-            .await {
-                Ok(fh) => fh,
-                Err(err) => { return req.reply_error(err.get_os_error().unwrap()); }
-            };
+        let fh = match self.connection.as_ref().read().await.get().await {
+            Err(_) => {
+                error!("No connection available");
+                return req.reply_error(libc::EIO);
+            }
+            Ok(conn) => {
+                trace!("Creating new file handle");
+                match self
+                    .file_handles
+                    .insert_new_fh(
+                        &conn,
+                        &self.exchange,
+                        &routing_key,
+                        op.flags(),
+                        &self.write_options,
+                    )
+                    .await {
+                        Ok(fh) => fh,
+                        Err(err) => { return req.reply_error(err.get_os_error().unwrap()); }
+                    }
+            }
+        };
 
         trace!("New file handle {}", fh);
         let mut out = OpenOut::default();
@@ -687,39 +695,38 @@ mod debug{
     }
 }
 
-impl Drop for Rabbit {
-    /// Close the RabbitMQ connection
-    fn drop(&mut self) {
-        info!("Shutting down filesystem");
-        let conn = self.connection.clone();
-        info!("Got connection");
-        let close = tokio::task::spawn(
-            async move {
-                conn.write().await.close(0, "Normal Shutdown").await.expect("close");
-                let state = conn.read().await.status().state();
-                state
-            });
-        let state = futures::executor::block_on(close).expect("Closing connection");
+// impl Drop for Rabbit {
+//     /// Close the RabbitMQ connection
+//     fn drop(&mut self) {
+//         info!("Shutting down filesystem");
+//         let conn = self.connection.clone();
+//         info!("Got connection");
+//         let close = tokio::task::spawn(
+//             async move {
+//                 conn.write().await.close(0, "Normal Shutdown").await.expect("close");
+//                 let state = conn.read().await.status().state();
+//                 state
+//             });
+//         let state = futures::executor::block_on(close).expect("Closing connection");
 
-        match state {
-            // Connection is already closed. This is good
-            lapin::ConnectionState::Closed => {
-                info!("Connection closed");
-            }
-            // Connection is closing. Will close someday? This is fine
-            lapin::ConnectionState::Closing => {
-                warn!("Connection closing but not closed");
-            }
-            // Failed to close, but what are going to do about it?
-            lapin::ConnectionState::Error => {
-                error!("Error closing connection");
-            }
-            // The other states are about opening. These are
-            // impossible to get here.
-            _ => {
-                panic!("Unable to close connection")
-            }
-        };
-
-    }
-}
+//         match state {
+//             // Connection is already closed. This is good
+//             lapin::ConnectionState::Closed => {
+//                 info!("Connection closed");
+//             }
+//             // Connection is closing. Will close someday? This is fine
+//             lapin::ConnectionState::Closing => {
+//                 warn!("Connection closing but not closed");
+//             }
+//             // Failed to close, but what are going to do about it?
+//             lapin::ConnectionState::Error => {
+//                 error!("Error closing connection");
+//             }
+//             // The other states are about opening. These are
+//             // impossible to get here.
+//             _ => {
+//                 panic!("Unable to close connection")
+//             }
+//         };
+// }
+// }
