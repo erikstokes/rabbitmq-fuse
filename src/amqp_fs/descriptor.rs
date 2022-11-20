663510b357ece492a7b4cd052853a807d8b86694
Erik Stokes <stokes.erik@gmail.com>
2022-11-20 16:41:03 -0500

    Remove code hacks

--- src/amqp_fs/descriptor.rs
+++ src/amqp_fs/descriptor.rs
@@ -122,13 +122,13 @@ impl FileHandle {
         );
 
 
-        let channel_conf = connection.configuration().clone();
-        channel_conf.set_frame_max(4096);
+        // let channel_conf = connection.configuration().clone();
+        // channel_conf.set_frame_max(4096);
 
         let channel = if opts.open_timeout_ms > 0 {
             let channel_open = tokio::time::timeout(
                 tokio::time::Duration::from_millis(opts.open_timeout_ms),
-                connection.create_channel_with_config(channel_conf));
+                connection.create_channel());
             match channel_open.await {
                 Ok(channel) => channel.unwrap(),
                 Err(_) => {
