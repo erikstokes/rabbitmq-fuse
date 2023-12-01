a87345534501105847a31b16e2e1855ca3ca8acd
Erik Stokes <stokes.erik@gmail.com>
2023-12-01 11:14:37 -0500

    Unused import

--- rabbitmq-fuse/src/amqp_fs/rabbit/amqprs/command.rs
+++ rabbitmq-fuse/src/amqp_fs/rabbit/amqprs/command.rs
@@ -24,19 +24,37 @@ impl EndpointCommand for Command {
             "localhost".to_string(),
         )
         .into_diagnostic()?;
-        let credentials =
-            if let Some(super::super::options::AuthMethod::Plain) = args.options.amqp_auth {
-                let plain = &args.options.plain_auth;
-                plain.try_into().into_diagnostic()?
-            } else {
-                miette::bail!("Only plain authentication is supported");
-            };
+        let credentials = if let Some(super::super::options::AuthMethod::Plain) =
+            args.options.amqp_auth
+        {
+            let plain = &args.options.plain_auth;
+            plain.try_into().into_diagnostic()?
+        } else {
+            miette::bail!("Only plain authentication is supported with the 'amqprs' endpoint. Consider using the default 'rabbit' endpoint");
+        };
         let opener = super::connection::Opener::new(
             &args.rabbit_addr.parse().into_diagnostic()?,
             credentials,
             tls,
         );
         let pool = ConnectionPool::builder(opener).build().into_diagnostic()?;
+        // if self.args.options.immediate_connection {
+        //     tracing::info!("Requested immediate connection test");
+        //     let _: Result<()> = futures::executor::block_on(async {
+        //         tracing::debug!("Opening test connection");
+        //         let conn = pool.get().await.into_diagnostic().with_context(|| {
+        //             format!("Failed to get connection to {}", self.args.rabbit_addr)
+        //         })?;
+        //         panic!("here");
+        //         miette::ensure!(
+        //             conn.open_channel(None).await.is_ok(),
+        //             "Failed to get connection to {}",
+        //             self.args.rabbit_addr
+        //         );
+        //         Ok(())
+        //     });
+        // }
+
         Ok(Self::Endpoint::new(
             pool,
             &args.exchange,
