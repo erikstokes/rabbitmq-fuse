e540ced38347fceb3c92f8f6693ff532d626a9a8
Erik Stokes <stokes.erik@gmail.com>
2023-12-13 00:28:04 -0500

    Use `lld` linker for coverage
    Otherwise this uses a lot of memory

--- coverage.sh
+++ coverage.sh
@@ -4,7 +4,7 @@
 
 cargo install grcov
 cargo clean
-RUST_LOG=trace CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage -Cdebug-assertions=no' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test --features=lapin-pool/deadpool -- --include-ignored
+RUST_LOG=trace CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage -Cdebug-assertions=no -Clink-arg=-fuse-ld=lld' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test --features=lapin-pool/deadpool -- --include-ignored
 ~/.cargo/bin/grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html --excl-br-line "unreachable!" --excl-line "unreachable!"
 
 # cleanup the temp files
