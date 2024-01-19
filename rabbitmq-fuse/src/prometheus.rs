c8270d53ca2de14289f62259cef5cd4b378154c4
Erik Stokes <stokes.erik@gmail.com>
2024-01-19 11:59:31 -0500

    Use axum for prometheus client instead of hyper
    Hyper 1.0 is too complicated for me to understand

--- rabbitmq-fuse/src/prometheus.rs
+++ rabbitmq-fuse/src/prometheus.rs
@@ -4,18 +4,11 @@ use prometheus_client::{encoding::text::encode, metrics::counter::Counter, regis
 
 use lazy_static::lazy_static;
 
-use hyper::{
-    service::{make_service_fn, service_fn},
-    Body, Request, Response, Server,
-};
+use axum::{extract::State, routing::get, Router};
 use std::{
-    future::Future,
-    io,
     net::{IpAddr, Ipv4Addr, SocketAddr},
-    pin::Pin,
     sync::Arc,
 };
-use tokio::signal::unix::{signal, SignalKind};
 
 use tracing::info;
 
@@ -24,50 +17,27 @@ lazy_static! {
 }
 
 /// Start a HTTP server to report metrics.
-pub async fn start_metrics_server(metrics_addr: SocketAddr, registry: Registry) {
-    let mut shutdown_stream = signal(SignalKind::terminate()).unwrap();
-
+pub async fn start_metrics_server(
+    metrics_addr: SocketAddr,
+    registry: Registry,
+) -> std::io::Result<()> {
     info!("Starting metrics server on {metrics_addr}");
-
     let registry = Arc::new(registry);
-    Server::bind(&metrics_addr)
-        .serve(make_service_fn(move |_conn| {
-            let registry = registry.clone();
-            async move {
-                let handler = make_handler(registry);
-                Ok::<_, io::Error>(service_fn(handler))
-            }
-        }))
-        .with_graceful_shutdown(async move {
-            shutdown_stream.recv().await;
-        })
-        .await
-        .unwrap();
+    let metrics_app = Router::new()
+        .route("/metrics", get(handler))
+        .with_state(registry);
+    let listener = tokio::net::TcpListener::bind(metrics_addr).await?;
+    axum::serve(listener, metrics_app).await?;
+    info!("Metrics server shutting down");
+    Ok(())
 }
 
-/// This function returns a HTTP handler (i.e. another function)
-pub fn make_handler(
-    registry: Arc<Registry>,
-) -> impl Fn(Request<Body>) -> Pin<Box<dyn Future<Output = io::Result<Response<Body>>> + Send>> {
-    // This closure accepts a request and responds with the OpenMetrics encoding of our metrics.
-    move |_req: Request<Body>| {
-        let reg = registry.clone();
-        Box::pin(async move {
-            let mut buf = String::new();
-            encode(&mut buf, &reg.clone())
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))
-                .map(|_| {
-                    let body = Body::from(buf);
-                    Response::builder()
-                        .header(
-                            hyper::header::CONTENT_TYPE,
-                            "application/openmetrics-text; version=1.0.0; charset=utf-8",
-                        )
-                        .body(body)
-                        .unwrap()
-                })
-        })
-    }
+/// Encode the current metrics to a string and return it as a reply to
+/// the prometheus scrape
+async fn handler(State(registry): State<Arc<Registry>>) -> String {
+    let mut buf = String::new();
+    encode(&mut buf, &registry).unwrap();
+    buf
 }
 
 /// Setup a default registry and start a server on port 8001 to be scraped.
